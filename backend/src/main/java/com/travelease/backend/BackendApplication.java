5d8b13f08d3879d14c06bd6e92de3280111010a5
vedp18 <ved09003@gmail.com>
2026-07-06 14:06:41 +0530

    Added Bearer auth fo swagger in BackendApplication.java.

--- backend/src/main/java/com/travelease/backend/BackendApplication.java
+++ backend/src/main/java/com/travelease/backend/BackendApplication.java
@@ -1,14 +1,35 @@
 package com.travelease.backend;
 
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TravelEase API",
+                version = "1.0",
+                description = "TravelEase Backend APIs"
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        description = "Enter JWT token with Bearer prefix. Example: Bearer eyJhbGciOiJIUzI1NiJ9..."
+)
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
