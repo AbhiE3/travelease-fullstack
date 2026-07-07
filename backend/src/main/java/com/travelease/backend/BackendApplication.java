31781c6e043abc06933cd581550dab2571222f6a
Neeharika S <neeharika.srinivasan@gmail.com>
2026-07-08 05:14:19 +0530

    Refactor OpenAPI configuration: move to BackendApplication and remove OpenApiConfig class; update application.properties for Swagger UI sorting

--- backend/src/main/java/com/travelease/backend/BackendApplication.java
+++ backend/src/main/java/com/travelease/backend/BackendApplication.java
@@ -1,13 +1,36 @@
 package com.travelease.backend;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	private static final String BEARER_AUTH = "bearerAuth";
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI travelEaseOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("TravelEase API")
+						.version("v1"))
+				.components(new Components()
+						.addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+								.name(BEARER_AUTH)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
 	}
 
 }
